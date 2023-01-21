eb4d6395bcfafaf43d2fc2a81bf83253a1aa3879
sg.jiri <sg.jiri@yahoo.fr>
2023-01-21 17:56:21 +0100

    Header3

---
+++ script.js
@@ -0,0 +1,6 @@
+var btBurger = document.getElementById('burger');
+var menuMobil = document.getElementById('menu')
+btBurger.addEventListener('click', function()
+{
+    menuMobil.classList.toggle('active')
+    })
