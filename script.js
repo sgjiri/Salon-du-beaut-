827a98a9550467da27d82dd4a29cf6d4cede72bd
sg.jiri <sg.jiri@yahoo.fr>
2023-01-23 12:06:51 +0100

    Menu

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var btBurger = document.getElementById('burger');
-var menuMobil = document.getElementById('menu')
+var menuMobil = document.getElementById('menu');
+var croix = document.getElementById('croix')
+var  btPrestations= document.getElementById('prestations');
+var sousmenu = document.getElementById('sousmenu');
 btBurger.addEventListener('click', function()
 {
     menuMobil.classList.toggle('active')
+    });
+
+croix.addEventListener('click', function(){
+    menuMobil.classList.remove('active')
+});
+
+
+if(menuMobil.class = 'active')
+{
+    btPrestations.addEventListener('click', function(){
+        sousmenu.classList.toggle('active')
     })
+    
+};
