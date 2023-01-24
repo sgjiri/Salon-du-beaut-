e7ee66fb724786d5b6a7c1f5548ed03d429a29b0
sg.jiri <sg.jiri@yahoo.fr>
2023-01-24 12:02:15 +0100

    Section1

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let menuMobil = document.getElementById('menu');
 let croix = document.getElementById('croix')
 let  btTriangle= document.getElementById('triangle');
 let sousmenu = document.getElementById('sousmenu');
+let croix2 = document.getElementById('croix2')
+let fleche =document.getElementById('fleche')
 btBurger.addEventListener('click', function()
 {
     menuMobil.classList.toggle('active')
@@ -13,11 +15,15 @@ croix.addEventListener('click', function(){
 });
 
 
-
-menuMobil.class = ('active')
-{
-    btTriangle.addEventListener('click', function(){
+btTriangle.addEventListener('click',function(){
         sousmenu.classList.toggle('active')
-    })
-    
-};
+    });
+
+croix2.addEventListener('click', function(){
+    sousmenu.classList.remove('active');
+    menuMobil.classList.remove('active');
+});
+
+fleche.addEventListener('click', function(){
+    sousmenu.classList.remove('active');
+})
