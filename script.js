574c1d03b06fdee4beddf7d9561880011f1d2d0f
sg.jiri <sg.jiri@yahoo.fr>
2023-01-25 16:30:49 +0100

    footer

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ let menuMobil = document.getElementById('menu');
 let croix = document.getElementById('croix')
 let  btTriangle= document.getElementById('triangle');
 let sousmenu = document.getElementById('sousmenu');
-let croix2 = document.getElementById('croix2')
-let fleche =document.getElementById('fleche')
+let croix2 = document.getElementById('croix2');
+let fleche =document.getElementById('fleche');
+let divFomulaire = document.getElementById('fomulaire');
+let divCarte = document.getElementById('carte');
+
+divCarte.style.height=divFomulaire.clientHeight + "px";
+
 btBurger.addEventListener('click', function()
 {
     menuMobil.classList.toggle('active')
